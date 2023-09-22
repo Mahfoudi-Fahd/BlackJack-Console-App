892d79046a5c6968c0eb1413610246ce0bd1da36
FAHD <mahfoudifahd55@gmail.com>
2023-09-22 15:59:00 +0100

    edit shuffle and create deck also start adding game rules

--- src/BlackJack.java
+++ src/BlackJack.java
@@ -3,7 +3,7 @@ import java.util.Random;
 
 public class BlackJack {
 
-    private class Card{
+    private static class Card{
         String value;
         String type;
 
@@ -11,52 +11,149 @@ public class BlackJack {
             this.value = value;
             this.type = type;
         }
+
         public String toString() {
             return value + "-" + type;
         }
+
+        public int getValue(int currentSum) {
+            if (isAce() && currentSum + 11 > 21) {
+                return 1;  // Ace should be 1 if it prevents going over 21
+            } else if ("AJQK".contains(value)) {
+                if (value.equals("A")) {
+                    return 11;
+                }
+                return 10;
+            }
+            return Integer.parseInt(value);
+        }
+
+        public boolean isAce(){
+            return value.equals("A");
+        }
     }
+
     ArrayList<Card> deck;
     Random random = new Random();
+
+
+//    Dealer
+    Card hiddenCard;
+    ArrayList<Card> dealerHand;
+    int dealerSum;
+    int dealerAceCount;
+
+//    Player
+    ArrayList<Card> playerHand;
+    int playerSum;
+    int playerAceCount;
 
     BlackJack(){
         startGame();
     }
     public void startGame(){
-        buildDeck();
+    // Deck
+        buildDeck("A","C");
         shuffleDeck();
+
+    // Dealer
+        dealerHand = new ArrayList<Card>();
+        dealerSum = 0;
+        dealerAceCount= 0;
+
+        hiddenCard = deck.remove(deck.size()-1);
+        dealerSum += hiddenCard.getValue(dealerSum);
+        dealerAceCount += hiddenCard.isAce() ? 1 : 0;
+
+        Card card = deck.remove(deck.size()-1);
+        dealerSum += card.getValue(dealerSum);
+        dealerAceCount += card.isAce() ? 1 : 0;
+        dealerHand.add(card);
+
+        System.out.println("\n-----------------------\nDealer\n-----------------------");
+        System.out.println(hiddenCard);
+        System.out.println(dealerHand);
+        System.out.println(dealerSum);
+        System.out.println(dealerAceCount);
+
+    //  Player
+        playerHand = new ArrayList<Card>();
+        playerSum = 0;
+        playerAceCount = 0;
+
+        for (int i = 0; i<2 ; i++){
+            card = deck.remove(deck.size()-1);
+            playerSum += card.getValue(dealerSum);
+            playerAceCount +=  card.isAce() ? 1: 0;
+            playerHand.add(card);
+        }
+        System.out.println("PLAYER :");
+        System.out.println(playerHand);
+        System.out.println(playerSum);
+        System.out.println(playerAceCount);
+
     }
-    public void buildDeck(){
+//    public void buildDeck(){
+//        deck = new ArrayList<Card>();
+//        String[] values = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
+//        String[] types = {"C", "D", "H", "S"};
+//
+//        for (int i = 0; i < types.length; i++) {
+//            for (int j = 0; j < values.length; j++) {
+//                Card card = new Card(values[j], types[i]);
+//                deck.add(card);
+//            }
+//        }
+//
+//        System.out.println("BUILD DECK:");
+//        System.out.println(deck);
+//    }
+
+
+    public void buildDeck(String minValue, String minType) {
         deck = new ArrayList<Card>();
         String[] values = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
         String[] types = {"C", "D", "H", "S"};
 
+        boolean startAdding = false;
+
         for (int i = 0; i < types.length; i++) {
             for (int j = 0; j < values.length; j++) {
-                Card card = new Card(values[j], types[i]);
-                deck.add(card);
+                if (types[i].equals(minType) && values[j].equals(minValue)) {
+                    startAdding = true;
+                }
+
+                if (startAdding) {
+                    Card card = new Card(values[j], types[i]);
+                    deck.add(card);
+                }
             }
         }
 
-        System.out.println("BUILD DECK:");
+        System.out.println("BUILD DECK AFTER " + minValue + "-" + minType + ":");
         System.out.println(deck);
     }
+
+
+
+
+
 
 
     public void  shuffleDeck(){
 
         int deckSize = deck.size();
 
-        for (int i = 0; i < deckSize; i++) {
+        for (int i = 0; i <100 ; i++) {
 
-            int j = random.nextInt(deckSize);
+            int randomIndex = random.nextInt(deckSize); //generate Random
+            System.out.println(randomIndex);
+            Card randomCard = deck.get(randomIndex); //get
 
-            Card temp = deck.get(i);
-            deck.set(i, deck.get(j));
-            deck.set(j, temp);
+            deck.remove(randomIndex); //remove
 
-
-
-            }
+            deck.add(0, randomCard); //add to the top
+        }
         System.out.println("AFTER SHUFFLE");
         System.out.println(deck);
     }
